1a84aefd0f7421d1af07f98b806f72f55f3690ed
GYegor <yahor2011@gmail.com>
2020-10-11 13:54:50 +0300

    feat: create endpoints for User

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,34 @@
+const db = require('../../db/inMemoryDB');
+const User = require('./user.model');
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return db.users;
 };
 
-module.exports = { getAll };
+const getById = async id => {
+  const user = db.users.find(u => u.id === id);
+  if (!user) {
+    throw new Error('User not found');
+  }
+  return user;
+};
+
+const add = async body => {
+  const newUser = new User(body);
+  db.users.push(newUser);
+  return newUser.id;
+};
+
+const update = async (id, body) => {
+  const userIndex = db.users.findIndex(user => user.id === id);
+  db.users[userIndex] = { ...db.users[userIndex], ...body };
+  // Object.assign(db.users[userIndex], body);
+  return id;
+};
+
+const remove = async id => {
+  const userIndex = db.users.findIndex(user => user.id === id);
+  db.users.splice(userIndex, 1);
+};
+
+module.exports = { getAll, getById, add, update, remove };
